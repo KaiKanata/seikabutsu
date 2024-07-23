b1f75940d8eb56fae3e2fb2f13803d432677b18e
EC2 Default User <kanaka.kai0827@gmail.com>
2024-07-23 18:07:43 +0900

    投稿一覧

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_','-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Colleview</title>
+        <!--Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nuito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>List</h1>
+        <div class='posts'>
+            @foreach($posts as $post)
+            <div class='post'>
+                <h2 class='title'>{{ $post->title }}</h2>
+                <img src="{{ $post->image_path }}" alt="{{ $post->title }}">
+                <p class='body'>{{ $post->body }}</p>
+            </div>
+            @endforeach 
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
+    </body>
+</html>
